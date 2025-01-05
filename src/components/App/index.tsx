83b699a9e9e15f7b040ec8c3e0975eef1cb31ac0
Ben Rogers <ben-r@hotmail.co.uk>
2025-01-05 13:29:29 +0000

    feat: TanStack Query

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,38 @@
+import { useQuery } from '@tanstack/react-query';
+
 import './App.css';
 
 export default function App() {
   return (
-    <div className="app">
+    <main className="app">
       <header className="app-header">
         <h1>Hello World</h1>
       </header>
+      <QueryExample />
+    </main>
+  );
+}
+
+function QueryExample() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ['queryExample'],
+    queryFn: () =>
+      fetch('https://api.github.com/repos/TanStack/query').then((res) =>
+        res.json()
+      ),
+  });
+
+  if (isPending) return 'Loading...';
+
+  if (error) return 'An error has occurred: ' + error.message;
+
+  return (
+    <div className="query-example">
+      <h1>{data.name}</h1>
+      <p>{data.description}</p>
+      <strong>👀 {data.subscribers_count}</strong>{' '}
+      <strong>✨ {data.stargazers_count}</strong>{' '}
+      <strong>🍴 {data.forks_count}</strong>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './components/App';
 
 const rootElement = document.getElementById('root');
+const queryClient = new QueryClient();
+
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </StrictMode>
   );
 }
